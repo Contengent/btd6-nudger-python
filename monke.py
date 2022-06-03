6fe0e784aaa8144130687a44465aaba94efa14c4
Contengent <103345406+Contengent@users.noreply.github.com>
2022-06-03 16:24:47 -0400

    Add files via upload

---
+++ monke.py
@@ -0,0 +1,25 @@
+# Imports
+from pynput.keyboard import Key, Listener
+from pynput.mouse import Controller
+
+# simplify Controller()
+mouse = Controller()
+
+# I honestly don't know what any of this does lol
+def movie(key):
+    def contral(kii, x, y):
+        if key == kii:
+            mouse.move(x, y)
+
+    contral(Key.up, 0, -1)
+    contral(Key.down, 0, 1)
+    contral(Key.left, -1, 0)
+    contral(Key.right, 1, 0)  
+
+    if key == Key.delete: 
+        return False
+
+# Collect all event until released
+with Listener(on_press = movie) as listener:
+    listener.join()
+
